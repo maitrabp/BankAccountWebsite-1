1be881fbca7b3ac53a00c68f334a9bbec3500ee8
maitrapatel <maitrabp@umich.edu>
2020-09-25 19:23:44 -0400

    configchanges

--- addaccount.php
+++ addaccount.php
@@ -1,8 +1,8 @@
 <?php
-define('DB_SERVER', '127.0.0.1:3360');
-define('DB_USERNAME', 'root');
-define('DB_PASSWORD', '');
-define('DB_DATABASE', 'bank2');
+define('DB_SERVER', 'us-cdbr-east-02.cleardb.com');
+define('DB_USERNAME', 'b9e3936e2f703f');
+define('DB_PASSWORD', '9e11b5b8');
+define('DB_DATABASE', 'heroku_cdb43a852bcdbe9');
 $db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
 //echo $id;
 session_start();
